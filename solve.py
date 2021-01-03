6ccf524467dd9b765647582edc8462abcff72192
jlui17 <justinlui17@gmail.com>
2021-01-02 20:46:04 -0800

    #2 Added: - check_valid - find_empty - solve (uses backtracking search to solve puzzle)

--- solve.py
+++ solve.py
@@ -1,16 +1,17 @@
 puzzle = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 0, 4, 0, 0, 0],
+    [7, 0, 1, 0, 0, 0, 3, 0, 0],
+    [0, 0, 5, 0, 0, 0, 0, 0, 9],
+    [0, 0, 0, 0, 0, 0, 0, 5, 0],
+    [9, 3, 0, 6, 0, 0, 0, 0, 8],
+    [0, 0, 0, 8, 0, 0, 0, 0, 1],
+    [1, 4, 0, 2, 0, 0, 8, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 4],
+    [0, 0, 0, 0, 9, 0, 7, 6, 0],
 ]
 
 
+# checks if v is not in the specified row
 def check_horizontal(y, v):
     for x in range(9):
         if v == puzzle[y][x]:
@@ -18,6 +19,7 @@ def check_horizontal(y, v):
     return True
 
 
+# checks if v is not in the specified column
 def check_vertical(x, v):
     for y in range(9):
         if v == puzzle[y][x]:
@@ -25,6 +27,7 @@ def check_vertical(x, v):
     return True
 
 
+# checks if v is not in the specified square
 def check_square(top_left_x, top_left_y, v):
     for offset_y in range(3):
         for offset_x in range(3):
@@ -33,6 +36,42 @@ def check_square(top_left_x, top_left_y, v):
     return True
 
 
+# checks if we can put v into that spot in the puzzle
+def check_valid(x, y, v):
+    top_left_x, top_left_y = x//3*3, y//3*3
+    return check_horizontal(y, v) and check_vertical(x, v) and check_square(top_left_x, top_left_y, v)
+
+
+# find empty spot
+def find_empty():
+    for y in range(9):
+        for x in range(9):
+            if puzzle[y][x] == 0:
+                return y, x
+
+
+# solve the puzzle
+def solve():
+    empty = find_empty()
+    # base case
+    if not empty:
+        return True
+    else:
+        y, x = empty
+
+    for v in range(1, 10):
+        if check_valid(x, y, v):
+            puzzle[y][x] = v
+
+            if solve():
+                return True
+
+            puzzle[y][x] = 0
+
+    return False
+
+
+# print puzzle
 def print_puzzle(grid):
     output = ""
     for i in range(9):
@@ -42,4 +81,7 @@ def print_puzzle(grid):
     print(output)
 
 
+print_puzzle(puzzle)
+print("\n------------------------------")
+solve()
 print_puzzle(puzzle)
