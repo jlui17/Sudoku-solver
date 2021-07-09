b73cc5098bdfe0fa6542604007c9110be3b4ba73
jlui17 <justinlui17@gmail.com>
2021-07-08 22:34:43 -0700

    #6 Added: - changed solve file into a class - added interactable GUI using pygame based off Tech With Tim tutorial

---
+++ gui.py
@@ -0,0 +1,192 @@
+import solve
+import pygame
+pygame.font.init()
+
+
+class Grid:
+    board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    def __init__(self, rows, cols, width, height):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
+        self.height = height
+        self.model = None
+        self.selected = None
+
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
+    def place(self, val):
+        row, col = self.selected
+        self.cubes[row][col].set(val)
+        self.update_model()
+        self.board[row][col] = val
+
+    def draw(self, win):
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
+            pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
+
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].draw(win)
+
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
+                self.cubes[i][j].selected = False
+
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
+
+    def clear(self):
+        for i in range(len(self.cubes)):
+            for j in range(len(self.cubes)):
+                self.cubes[i][j].set(0)
+                self.board[i][j] = 0
+
+    def click(self, pos):
+        """
+        :param: pos
+        :return: (row, col)
+        """
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return int(y), int(x)
+        else:
+            return None
+
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+
+class Cube:
+    rows = 9
+    cols = 9
+
+    def __init__(self, value, row, col, width ,height):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.width = width
+        self.height = height
+        self.selected = False
+        self.blank = 0
+
+    def draw(self, win):
+        fnt = pygame.font.SysFont("comicsans", 40)
+
+        gap = self.width / 9
+        x = self.col * gap
+        y = self.row * gap
+
+        if self.value == 0:
+            text = fnt.render("", 1, (128,128,128))
+            win.blit(text, (x+5, y+5))
+        elif self.value != 0:
+            text = fnt.render(str(self.value), 1, (0, 0, 0))
+            win.blit(text, (x + (gap/2 - text.get_width()/2), y + (gap/2 - text.get_height()/2)))
+
+        if self.selected:
+            pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
+
+    def set(self, val):
+        self.value = val
+
+
+def redraw_window(win, board):
+    win.fill((255,255,255))
+    # Draw grid and board
+    board.draw(win)
+
+
+def main():
+    win = pygame.display.set_mode((540,600))
+    pygame.display.set_caption("Sudoku")
+    board = Grid(9, 9, 540, 540)
+    run = True
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                i, j = board.selected
+                if event.key == pygame.K_1 and (i or i ==0):
+                    board.cubes[i][j].set(1)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_2 and (i or i ==0):
+                    board.cubes[i][j].set(2)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_3 and (i or i ==0):
+                    board.cubes[i][j].set(3)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_4 and (i or i ==0):
+                    board.cubes[i][j].set(4)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_5 and (i or i ==0):
+                    board.cubes[i][j].set(5)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_6 and (i or i ==0):
+                    board.cubes[i][j].set(6)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_7 and (i or i ==0):
+                    board.cubes[i][j].set(7)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_8 and (i or i ==0):
+                    board.cubes[i][j].set(8)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_9 and (i or i ==0):
+                    board.cubes[i][j].set(9)
+                    board.place(board.cubes[i][j].value)
+                if event.key == pygame.K_BACKSPACE:
+                    board.place(board.cubes[i][j].blank)
+                    board.board[i][j] = 0
+                if event.key == pygame.K_c:
+                    board.clear()
+                if event.key == pygame.K_RETURN:
+                    solver = solve.Solver(board.board)
+                    solver.solve(solver.puzzle)
+                    solved = solver.puzzle
+                    for i in range(9):
+                        for j in range(9):
+                            board.cubes[i][j].set(solved[i][j])
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                clicked = board.click(pos)
+                if clicked:
+                    board.select(clicked[0], clicked[1])
+
+        redraw_window(win, board)
+        pygame.display.update()
+
+
+main()
+pygame.quit()
--- solve.py
+++ solve.py
@@ -1,120 +1,81 @@
-empty_puzzle = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+class Solver:
 
-puzzle = [
-    [0, 0, 0, 7, 0, 4, 0, 0, 0],
-    [7, 0, 1, 0, 0, 0, 3, 0, 0],
-    [0, 0, 5, 0, 0, 0, 0, 0, 9],
-    [0, 0, 0, 0, 0, 0, 0, 5, 0],
-    [9, 3, 0, 6, 0, 0, 0, 0, 8],
-    [0, 0, 0, 8, 0, 0, 0, 0, 1],
-    [1, 4, 0, 2, 0, 0, 8, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 4],
-    [0, 0, 0, 0, 9, 0, 7, 6, 0],
-]
-
-puzzle1 = [
-    [0, 5, 3, 2, 0, 0, 4, 0, 8],
-    [0, 0, 0, 0, 0, 0, 0, 9, 0],
-    [0, 0, 0, 6, 0, 0, 0, 0, 7],
-    [0, 0, 0, 8, 0, 3, 0, 0, 0],
-    [0, 0, 2, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 6, 4, 9, 0, 0],
-    [6, 0, 0, 0, 0, 5, 0, 7, 2],
-    [0, 0, 5, 0, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 0, 0, 8, 0, 3]
-]
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
 
 
-# checks if v is not in the specified row
-def check_horizontal(y, v, grid):
-    for x in range(9):
-        if v == grid[y][x]:
-            return False
-    return True
-
-
-# checks if v is not in the specified column
-def check_vertical(x, v, grid):
-    for y in range(9):
-        if v == grid[y][x]:
-            return False
-    return True
-
-
-# checks if v is not in the specified square
-def check_square(top_left_x, top_left_y, v, grid):
-    for offset_y in range(3):
-        for offset_x in range(3):
-            if v == grid[top_left_y + offset_y][top_left_x + offset_x]:
-                return False
-    return True
-
-
-# checks if we can put v into that spot in the puzzle
-def check_valid(x, y, v, grid):
-    top_left_x, top_left_y = x//3*3, y//3*3
-    return check_horizontal(y, v, grid) and check_vertical(x, v, grid) and check_square(top_left_x, top_left_y, v, grid)
-
-
-# find empty spot
-def find_empty(grid):
-    for y in range(9):
+    # checks if v is not in the specified row
+    def check_horizontal(self, y, v, grid):
         for x in range(9):
-            if grid[y][x] == 0:
-                return y, x
-
-
-# solve the puzzle
-def solver(grid):
-    empty = find_empty(grid)
-    # base case
-    if not empty:
+            if v == grid[y][x]:
+                return False
         return True
-    else:
-        y, x = empty
-
-    for v in range(1, 10):
-        if check_valid(x, y, v, grid):
-            grid[y][x] = v
-
-            if solver(grid):
-                return True
-
-            grid[y][x] = 0
-
-    return False
 
 
-# print puzzle
-def print_puzzle(grid):
-    output = ""
-    for i in range(9):
-        if i % 3 == 0 and not i == 0:
-            output += "---------------------\n"
-        for j in range(9):
-            if j % 3 == 0 and not j == 0:
-                output += "| "
-            output += (str(grid[i][j]) + " ")
-        output += "\n"
-    print(output)
+    # checks if v is not in the specified column
+    def check_vertical(self, x, v, grid):
+        for y in range(9):
+            if v == grid[y][x]:
+                return False
+        return True
 
 
-# print and solve puzzle
-def solve(grid):
-    print_puzzle(grid)
-    print("---------------------\n")
-    solver(grid)
-    print_puzzle(grid)
+    # checks if v is not in the specified square
+    def check_square(self, top_left_x, top_left_y, v, grid):
+        for offset_y in range(3):
+            for offset_x in range(3):
+                if v == grid[top_left_y + offset_y][top_left_x + offset_x]:
+                    return False
+        return True
 
 
-solve(puzzle1)
+    # checks if we can put v into that spot in the puzzle
+    def check_valid(self, x, y, v, grid):
+        top_left_x, top_left_y = x//3*3, y//3*3
+        return self.check_horizontal(y, v, grid) and self.check_vertical(x, v, grid) and self.check_square(top_left_x, top_left_y, v, grid)
+
+
+    # find empty spot
+    def find_empty(self, grid):
+        for y in range(9):
+            for x in range(9):
+                if grid[y][x] == 0:
+                    return y, x
+
+
+    # solve the puzzle
+    def solve(self, grid):
+        empty = self.find_empty(grid)
+        # base case
+        if not empty:
+            return True
+        else:
+            y, x = empty
+
+        for v in range(1, 10):
+            if self.check_valid(x, y, v, grid):
+                grid[y][x] = v
+
+                if self.solve(grid):
+                    return True
+
+                grid[y][x] = 0
+
+        return False
+
+
+    # print puzzle
+    def print_puzzle(self, grid):
+        output = ""
+        for i in range(9):
+            if i % 3 == 0 and not i == 0:
+                output += "---------------------\n"
+            for j in range(9):
+                if j % 3 == 0 and not j == 0:
+                    output += "| "
+                output += (str(grid[i][j]) + " ")
+            output += "\n"
+        print(output)
+
+
+
